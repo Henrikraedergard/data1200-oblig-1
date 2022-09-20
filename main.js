8f33627c491b703a63e70af22247fae784bb3871
Henrik Rædergård <Henrikra@viken.no>
2022-09-20 16:37:29 +0200

    Små endringer

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
+// Henter tiden til datamaskinen
 let dag = new Date().getDay();
 let tid = new Date().getHours();
-let åpent = false
 
-console.log(dag, tid);
-
+// Hvis det er søndag
 if (dag > 0) {
-    if (dag == 6) {
-        if (tid >= 10 && tid < 17){
-        }
-    }
-    else if (tid > 10 && tid < 16) {
-        setÅpent()
-    }
+	// hvis det er lørdag
+	if (dag == 6) {
+		// hvis tiden på datamaskinen er mellom 10 og 16
+		if (tid >= 10 && tid < 17) {
+			setÅpent();
+		}
+
+		// Alle andre dager enn lørdag
+		// Hvis tiden er mellom 10 og 15
+	} else if (tid >= 10 && tid < 16) {
+		setÅpent();
+	}
 }
 
+// Endrer teksten til å si at butikken er åpen
 function setÅpent() {
-    const erÅpentEl = document.getElementById("erÅpent");
-    erÅpentEl.innerHTML = "Cafeen er <span title='Vi bruker din maskins lokal tid til å sjekke om cafeen er åpen' style='color: green;'>åpen</span"
-    console.log(erÅpentEl);
+	const erÅpentEl = document.getElementById("erÅpent");
+	erÅpentEl.innerHTML =
+		"Cafeen er <span title='Vi bruker din maskins lokal tid til å sjekke om cafeen er åpen' style='color: green;'>åpen</span";
 }
